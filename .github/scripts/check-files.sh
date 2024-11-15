0e49dc8d5eded597d31ad5058e2b6608caf7745f
arun-j0 <159032521+arun-j0@users.noreply.github.com>
2024-11-16 01:09:53 +0530

    cicd test2

--- .github/scripts/check-files.sh
+++ .github/scripts/check-files.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Handling Merge base commits
 if [[ $(git show --summary | grep 'Merge:') != "" ]]; then
     last_changes=$(git show --summary | grep Merge: | xargs | awk '{print($3".."$2)}')
@@ -8,3 +10,4 @@ else
     commit_id=$(git rev-parse HEAD)
     echo "Setting output for last non merged commit ${commit_id}"
     echo "::set-output name=files::$(git diff-tree --no-commit-id --name-only -r ${commit_id} | xargs)"
+fi  # <-- This is the missing `fi`
