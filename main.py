f51d6da4bb9ce01c2be4248188aeb9d5c0de30e3
arun-j0 <159032521+arun-j0@users.noreply.github.com>
2024-11-19 20:09:20 +0530

    chores: docker file updated for image embedding, cors added

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from routes import text_embedded
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(text_embedded.router, tags=["Text-embedded microservice"])
