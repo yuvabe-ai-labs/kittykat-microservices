1ef3a2e12225690928bc70d5a810191294488ca8
Keerthu-k <keerthana@yuvabe.com>
2024-11-19 14:14:41 +0530

    update the changes

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from routes import text_embedded
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins, but you can restrict this
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
 
 app.include_router(text_embedded.router, tags=["Text-embedded microservice"])
