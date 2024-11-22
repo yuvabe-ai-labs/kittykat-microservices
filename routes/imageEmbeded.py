e456d74f2dbc81411e8ada71e9a90275ea739865
naveenyuvabe <naveen@yuvabe.com>
2024-11-22 08:15:25 +0530

    86cx4cwda - [image_embedding_service] ADD: Response Model

--- routes/imageEmbeded.py
+++ routes/imageEmbeded.py
@@ -6,7 +6,7 @@ from PIL import Image
 import base64
 import io
 from pydantic import BaseModel
-from models.models import Base64ImageRequest
+from models.models import Base64ImageRequest, ImageEmbedResponse
 from services.image_search_utils import handle_image_embeddeding
 from services.image_process_utils import process_image_from_url
 from services.embed_utils import send_img_to_embed
@@ -17,7 +17,9 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-@router.post("/image/embed/file", summary="Embedding a Image")
+@router.post(
+    "/image/embed/file", summary="Embedding a Image", response_model=ImageEmbedResponse
+)
 async def embed_images(
     file: UploadFile = File(..., description="Image file to search by")
 ):
@@ -31,7 +33,11 @@ async def embed_images(
     return embedding
 
 
-@router.post("/image/embed/url", summary="Embedding a Imagen from url")
+@router.post(
+    "/image/embed/url",
+    summary="Embedding a Imagen from url",
+    response_model=ImageEmbedResponse,
+)
 async def embed_images(url: str):
     """The search-by-image  endpoint allows users to search for similar images by uploading an image file. It accepts an image file, the number of top similar items to return (top_k), and a list of namespaces to search within. The image is processed to extract its embedding, and then the system retrieves similar images by comparing the embedding across the provided namespaces."""
     if not url:
@@ -43,7 +49,11 @@ async def embed_images(url: str):
     return embedding
 
 
-@router.post("/image/embed/base64", summary="Embedding a base64 Image")
+@router.post(
+    "/image/embed/base64",
+    summary="Embedding a base64 Image",
+    response_model=ImageEmbedResponse,
+)
 async def embed_base64_image(request: Base64ImageRequest):
     """
     This endpoint allows users to search for similar images by providing an image in base64 format.
@@ -73,64 +83,3 @@ async def embed_base64_image(request: Base64ImageRequest):
         raise HTTPException(
             status_code=500, detail=f"An error occurred during image embedding: {e}"
         )
-
-
-# @router.post("/embedded-image-list", summary="Embedding multiple images")
-# async def embed_images(
-#     files: List[UploadFile] = File(..., description="List of image files to search by")
-# ):
-#     """The search-by-image endpoint allows users to search for similar images by uploading multiple image files.
-#     It accepts a list of image files and processes each image to extract its embedding.
-#     The response is a dictionary with the index of each image as the key and the embedding as the value.
-#     """
-
-#     if not files or len(files) == 0:
-#         raise HTTPException(
-#             status_code=400,
-#             detail="At least one image file is required for the search.",
-#         )
-
-#     # Dictionary to hold index and corresponding embedding
-#     embeddings_dict = {}
-
-#     for index, file in enumerate(files):
-#         # Validate that the file is an image
-#         if not file.content_type.startswith("image/"):
-#             raise HTTPException(
-#                 status_code=400,
-#                 detail=f"File at index {index} is not a valid image type.",
-#             )
-
-#         # Process the image to extract its embedding
-#         embedding = await handle_image_search(file)
-#         embeddings_dict[index] = embedding
-
-#     return embeddings_dict
-
-
-# @router.post("/embedded-image-multiple-urls", summary="Embedding images from URLs")
-# async def embed_images(urls: List[str]):
-#     """The search-by-image endpoint allows users to search for similar images by providing a list of image URLs.
-#     It processes each URL to extract its embedding and returns a dictionary with the index of the URL as the key
-#     and the embedding as the value."""
-
-#     if not urls or len(urls) == 0:
-#         raise HTTPException(
-#             status_code=400, detail="At least one image URL is required for the search."
-#         )
-
-#     # Dictionary to hold index and corresponding embedding
-#     embeddings_dict = {}
-
-#     for index, url in enumerate(urls):
-#         # Validate that the URL is not empty or malformed
-#         if not url.startswith("http"):
-#             raise HTTPException(
-#                 status_code=400, detail=f"URL at index {index} is not a valid URL."
-#             )
-
-#         # Process the image from the URL to extract its embedding
-#         embedding = await process_image_from_url(url)
-#         embeddings_dict[index] = embedding
-
-#     return embeddings_dict
