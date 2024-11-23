53ba4b8b5e3394a8280dd28675b52c9b292e8ef1
naveenyuvabe <naveen@yuvabe.com>
2024-11-23 11:06:13 +0530

    86cx4cwda - [image_embedding_service] Refactor: Schema and Exception Handlings

--- image_embedding_service/routes/imageEmbeded.py
+++ image_embedding_service/routes/imageEmbeded.py
@@ -1,75 +1,132 @@
 import logging
+import uuid
 from typing import Optional, Dict, Any, List
 from fastapi import APIRouter, HTTPException, File, UploadFile
 import numpy as np
 from PIL import Image
 import base64
 import io
-from pydantic import BaseModel, HttpUrl, ValidationError
-from models.models import UrlRequest, ImageEmbedResponse
+from pydantic import ValidationError
+from models.models import UrlRequest, ImageEmbedResponse, UrlValidator
 from services.image_search_utils import handle_image_embeddeding
 from services.image_process_utils import process_image_from_url
 from services.embed_utils import send_img_to_embed
-from exceptions.ImageEmbedExceptions import (
-    urlNotFoundException,
-    InvalidUrlException,
-    UnauthorizedUrlException,
-    ImageProcessingException,
-)
 import aiohttp
 
-
-class UrlValidator(BaseModel):
-    url: HttpUrl
-
-
+# Initialize router
 router = APIRouter()
 
+# Setup logger
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
 @router.post(
     "/image/embed/url",
-    summary="Embedding a Imagen from url",
+    summary="Embedding an Image from a URL",
     response_model=ImageEmbedResponse,
 )
 async def embed_images(request: UrlRequest):
-    """The search-by-image  endpoint allows users to search for similar images by uploading an image file. It accepts an image file, the number of top similar items to return (top_k), and a list of namespaces to search within. The image is processed to extract its embedding, and then the system retrieves similar images by comparing the embedding across the provided namespaces."""
+    """Endpoint to embed image from a provided URL, process it, and return image embeddings."""
 
     url = request.url
+    requestId = request.request_id
+    response_id = uuid.uuid4().hex
 
+    # Check if URL is provided
     if not url:
         logger.warning("URL not provided.")
-        raise InvalidUrlException("URL is required but not provided.")
+        return ImageEmbedResponse(
+                url=url,
+                request_id=requestId,
+                response_id=response_id,
+                ImageEmbeddings=[],
+                Message="URL is Empty."
+            )
+
     # Validate URL format
     try:
         UrlValidator(url=url)
     except ValidationError:
-        logger.warning("Invalid URL format provided.")
-        raise InvalidUrlException("Invalid URL format provided.")
+        logger.warning(f"Invalid URL format provided: {url}")
+        return ImageEmbedResponse(
+                        url=url,
+                        request_id=requestId,
+                        response_id=response_id,
+                        ImageEmbeddings=[],
+                        Message="Invalid URL format provided."
+                    )
 
     # Check URL accessibility
     async with aiohttp.ClientSession() as session:
         try:
             async with session.get(url) as response:
                 if response.status == 401:  # Unauthorized
-                    logger.warning("URL requires authentication.")
-                    raise UnauthorizedUrlException()
+                    logger.warning(f"URL requires authentication: {url}")
+                    return ImageEmbedResponse(
+                        url=url,
+                        request_id=requestId,
+                        response_id=response_id,
+                        ImageEmbeddings=[],
+                        Message="URL requires authentication."
+                    )
+                elif response.status == 403:  # Forbidden
+                    logger.warning(f"URL is forbidden: {url}")
+                    return ImageEmbedResponse(
+                        url=url,
+                        request_id=requestId,
+                        response_id=response_id,
+                        ImageEmbeddings=[],
+                        Message="URL is Forbidden."
+                    )
+                elif response.status == 404:  # Not Found
+                    logger.warning(f"URL is Not Found: {url}")
+                    return ImageEmbedResponse(
+                        url=url,
+                        request_id=requestId,
+                        response_id=response_id,
+                        ImageEmbeddings=[],
+                        Message="URL is URL is Not Found"
+                    )
                 elif response.status != 200:  # Other HTTP errors
-                    logger.warning(f"URL returned status code {response.status}.")
+                    logger.warning(
+                        f"URL returned status code {response.status} for: {url}"
+                    )
                     raise HTTPException(
                         status_code=response.status,
                         detail=f"URL returned status code {response.status}.",
                     )
+                    
+                    
         except aiohttp.ClientError as e:
-            logger.warning(f"Error accessing the URL: {e}")
-            raise InvalidUrlException("Could not access the provided URL.")
+            logger.warning(f"Error accessing the URL {url}: {e}")
+            return ImageEmbedResponse(
+                        url=url,
+                        request_id=requestId,
+                        response_id=response_id,
+                        ImageEmbeddings=[],
+                        Message=f"Error accessing the URL {url}: {e}.",
+                    )
+        
+        
 
     # Process image from URL
     try:
         embedding = await process_image_from_url(url)
-        return {"ImageEmbeddings": embedding}
+        logger.info(f"Successfully processed image from URL: {url}")
+        return ImageEmbedResponse(
+                        url=url,
+                        request_id=requestId,
+                        response_id=response_id,
+                        ImageEmbeddings=embedding,
+                        Message="Success."
+                    )
     except Exception as e:
-        logger.error(f"Failed to process image from URL: {e}")
-        raise ImageProcessingException("Failed to process image from URL.")
+        logger.error(f"Failed to process image from URL: {url}. Error: {e}")
+        return ImageEmbedResponse(
+                        url=url,
+                        request_id=requestId,
+                        response_id=response_id,
+                        ImageEmbeddings=[],
+                        Message="Failed to process image from URL."
+                    )
