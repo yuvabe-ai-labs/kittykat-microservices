c1a474aad69f4baf0a9c9c7cff22d4b7195a8814
naveenyuvabe <naveen@yuvabe.com>
2024-11-25 10:39:06 +0530

    86cx4cwmw - [text_embedding_service]Refactor : Exceptions folder

--- text_embedding_service/exceptions/TextEmbedExceptions.py
+++
@@ -1,6 +0,0 @@
-from fastapi import HTTPException, status
-
-
-class TextNotFoundException(HTTPException):
-    def __init__(self):
-        super().__init__(status_code=400, detail="Text it required for embeddings")
--- text_embedding_service/services/handle_textSearch.py
+++ text_embedding_service/services/handle_textSearch.py
@@ -2,15 +2,28 @@ import logging
 from fastapi import HTTPException
 from services.embed_utils import send_text_to_embed
 
+# Configure logger
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-async def handle_text_search(text: str):
+async def handle_text_search(search_query: str):
+    """
+    Processes a text search query and generates an embedding for the text.
+
+    Parameters:
+        search_query (str): The input text query for which an embedding is to be generated.
+
+    Returns:
+        dict: A dictionary containing the embedding of the input text.
+    """
     try:
-        embedding1 = await send_text_to_embed(text)
+        # Generate the embedding for the input text query
+        embedding1 = await send_text_to_embed(search_query)
         return embedding1  # Return the text embedding
+
     except Exception as e:
+        # Raise an HTTP exception with the error message
         raise HTTPException(
             status_code=500, detail=f"An error occurred during text search: {e}"
         )
