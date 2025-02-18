d55a988a21d1418103394d7b4be397877cd09a97
arun-j0 <arun2310kumar2002@gmail.com>
2025-02-18 14:57:27 +0530

    chore : added stage type prefix storage for image generation service

--- services/gcp.py
+++ services/gcp.py
@@ -4,6 +4,12 @@ from google.cloud import storage
 from google.cloud.exceptions import GoogleCloudError
 from config.gcp import bucket_prefix, bucket
 from config.logger import logger
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+stage_type = os.getenv("STAGE_TYPE")
 
 
 def upload_to_gcp(source_url: str, destination_blob_name: str) -> str:
@@ -26,7 +32,9 @@ def upload_to_gcp(source_url: str, destination_blob_name: str) -> str:
         )
 
         # Prefix the destination blob name with the bucket prefix
-        full_destination_blob_name = f"{bucket_prefix}/{destination_blob_name}"
+        full_destination_blob_name = (
+            f"{bucket_prefix}/{stage_type}/{destination_blob_name}"
+        )
 
         # Check if the blob already exists
         blob = bucket.blob(full_destination_blob_name)
