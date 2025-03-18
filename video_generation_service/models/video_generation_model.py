e9eb4c04434be6b1516d90b03e79d832d386208b
Abineshkumar77 <abineshkumar2504@gmail.com>
2025-03-18 12:26:43 +0530

    feat kling video generation

---
+++ video_generation_service/models/video_generation_model.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel, HttpUrl, field_validator
+from typing import List, Optional, Union
+
+class RunPredictionRequest(BaseModel):
+    prompt: str
+    start_image: Optional[str] = None
+    duration: Optional[int] = 5
+    aspect_ratio: Optional[str] = "16:9"
+    cfg_scale: Optional[float] = 0.5
+    negative_prompt: Optional[str] = ""
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "prompt": "A futuristic city with flying cars and neon lights",
+                "start_image": "https://replicate.delivery/mgxm/5de85319-a354-4178-a2b0-aab4a65fa480/start.png",
+                "duration": 5,
+                "aspect_ratio": "16:9",
+                "cfg_scale": 0.5,
+                "negative_prompt": "blurry, low-resolution"
+            }
+        }
+
+class RunPredictionResponse(BaseModel):
+    id: str
+
+class PredictionStatus(BaseModel):
+    id: str
+    status: str
+    output: Optional[Union[HttpUrl, List[HttpUrl]]] = None
+
+    @field_validator("output", mode="before")
+    def convert_to_list(cls, v):
+        if isinstance(v, str):
+            return [v]
+        return v
