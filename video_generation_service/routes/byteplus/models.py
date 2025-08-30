cfab4a7113df5f8de247a18eb5a4f7e3ab5d1681
Dhanush <iamdhanush02@gmail.com>
2025-08-30 07:56:37 +0530

    feat: integrate BytePlus video generation service with API and logging enhancements

---
+++ video_generation_service/routes/byteplus/models.py
@@ -0,0 +1,122 @@
+from typing import Annotated, Literal, Optional, Union
+from pydantic import BaseModel, Field, HttpUrl, field_serializer
+
+
+class BaseParams(BaseModel):
+    webhook_url: HttpUrl
+
+
+class Seedance_1_0_Lite_I2V_Params(BaseParams):
+    prompt: str = Field(
+        description="The text prompt to guide the video generation."
+    )
+    model: Literal["seedance-1-0-lite-i2v-250428"]
+    first_frame: HttpUrl = Field(
+        description="The URL of the first frame image."
+    )
+    last_frame: Optional[HttpUrl] = Field(
+        description="The URL of the last frame image."
+    )
+    resolution: Literal["480p", "720p", "1080p"] = Field(
+        default="720p",
+        description="The resolution of the output video."
+    )
+    ratio: Literal[
+        "16:9",
+        "4:3",
+        "1:1",
+        "3:4",
+        "9:16",
+        "21:9",
+        "adaptive"
+    ] = Field(
+        default="adaptive",
+        description="The aspect ratio of the output video."
+    )
+    duration: int = Field(
+        default=5,
+        ge=3,
+        le=12,
+        description="The duration of the output video"
+    )
+    framepersecond: Literal[24] = Field(
+        default=24,
+        description="The frame rate of the output video."
+    )
+    watermark: bool = Field(
+        default=False,
+        description="Whether to add a watermark to the output video."
+    )
+    seed: int = Field(
+        default=-1,
+        description="Random seed for video generation. If not provided, a random seed will be used."
+    )
+    camerafixed: bool = Field(
+        default=False,
+        description="Specifies whether to fix the camera."
+    )
+
+    @field_serializer("first_frame")
+    def serialize_website(self, v: HttpUrl, _info):
+        return str(v)
+
+
+class Seedance_1_0_Pro_Params(BaseParams):
+    prompt: str = Field(
+        description="The text prompt to guide the video generation."
+    )
+    model: Literal["seedance-1-0-pro-250528"]
+    first_frame: HttpUrl = Field(
+        description="The URL of the first frame image."
+    )
+    resolution: Literal["480p", "720p", "1080p"] = Field(
+        default="1080p",
+        description="The resolution of the output video."
+    )
+    ratio: Literal[
+        "16:9",
+        "4:3",
+        "1:1",
+        "3:4",
+        "9:16",
+        "21:9",
+        "adaptive"
+    ] = Field(
+        default="adaptive",
+        description="The aspect ratio of the output video."
+    )
+    duration: int = Field(
+        default=5,
+        ge=3,
+        le=12,
+        description="The duration of the output video"
+    )
+    framepersecond: Literal[24] = Field(
+        default=24,
+        description="The frame rate of the output video."
+    )
+    watermark: bool = Field(
+        default=False,
+        description="Whether to add a watermark to the output video."
+    )
+    seed: int = Field(
+        default=-1,
+        description="Random seed for video generation. If not provided, a random seed will be used."
+    )
+    camerafixed: bool = Field(
+        default=False,
+        description="Specifies whether to fix the camera."
+    )
+
+    @field_serializer("first_frame")
+    def serialize_website(self, v: HttpUrl, _info):
+        return str(v)
+
+
+BytePlusVideoGenerationRequest = Annotated[
+    Union[
+        Seedance_1_0_Pro_Params,
+        Seedance_1_0_Lite_I2V_Params
+    ],
+    Field(discriminator="model")
+]
