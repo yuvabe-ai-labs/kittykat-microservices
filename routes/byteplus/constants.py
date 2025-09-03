80978ba19fc0dc79968130c6f940e5c7c1cc539e
Dhanush <iamdhanush02@gmail.com>
2025-09-04 00:03:50 +0530

    feat: implement content filter handling in image generation and editing services

--- routes/byteplus/constants.py
+++ routes/byteplus/constants.py
@@ -9,3 +9,11 @@ BYTEPLUS_NFSW_ERROR_CODES = [
     "OutputImageSensitiveContentDetected",
     "OutputVideoSensitiveContentDetected"
 ]
+
+model_content_filters = {
+    "seededit-3-0-i2i-250628": "ep-20250903153516-x4rgf",
+    "seedance-1-0-lite-t2v-250428": "ep-20250903154616-2v745",
+    "seedance-1-0-lite-i2v-250428": "ep-20250903154548-9x5xc",
+    "seedream-3-0-t2i-250415": "ep-20250903154649-477dp",
+    "seedance-1-0-pro-250528": "ep-20250903154721-qxf8s",
+}
