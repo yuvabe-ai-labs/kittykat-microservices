99af49e3f2591c8926570eadf19380808214df38
Dhanush <iamdhanush02@gmail.com>
2025-09-12 02:53:18 +0530

    feat: add virtual try-on image generation functionality and related constants

--- image_generation_service/routes/gemini/router.py
+++ image_generation_service/routes/gemini/router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, status
 from core.utils import BaseApiResponse
 from core.models import ImageResponse
 from config.logger import logger
-from .models import GeminiImageGenerationRequest, GeminiImageEditRequest
+from .models import GeminiImageGenerationRequest, GeminiImageEditRequest, GeminiVirtualTryOnRequest
 from .service import GeminiService
 
 
@@ -46,6 +46,34 @@ async def edit_image(
     Edit an image using Gemini's image generation API.
     """
 
+    try:
+        gemini_service = GeminiService()
+
+        data = gemini_service.edit_image(request=request)
+
+        return BaseApiResponse(
+            status_code=status.HTTP_200_OK,
+            message="Image edited successfully",
+            data=data
+        )
+
+    except Exception as e:
+        logger.error(f"Error editing image: {e}")
+        return BaseApiResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            message="An error occurred while edit the image. Please try again later",
+            data=ImageResponse(
+                error=str(e),
+                is_nsfw_detected=False,
+            )
+        )
+
+
+@router.post("/vton", response_model=BaseApiResponse)
+async def generte_vton_image(request: GeminiVirtualTryOnRequest):
+    """
+    Virtual Try-On (VTON) image generation.
+    """
     try:
         gemini_service = GeminiService()
 
