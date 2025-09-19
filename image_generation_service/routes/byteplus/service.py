ff1faf00b88c5075c9f3e80503846de55cd7cc5a
Dhanush <iamdhanush02@gmail.com>
2025-09-20 01:15:34 +0530

    refactor: enhance error handling and logging in image generation methods

--- image_generation_service/routes/byteplus/service.py
+++ image_generation_service/routes/byteplus/service.py
@@ -3,6 +3,7 @@ import io
 
 import requests
 from byteplussdkarkruntime import Ark
+from byteplussdkarkruntime._exceptions import ArkBadRequestError
 from config.env import config
 from config.logger import logger
 from core.models import ImageResponse
@@ -21,86 +22,113 @@ class BytePlusService:
         )
 
     def generate_image(self, request: BytePlusImageGenerationRequest) -> ImageResponse:
-        model = request.model
+        try:
+            model = request.model
 
-        if request.content_filter_disabled:
-            logger.info(
-                "Changing model configuration: content moderation disabled")
+            if request.content_filter_disabled:
+                logger.info(
+                    "Changing model configuration: content moderation disabled")
 
-            filtered_model = model_content_filters.get(model, None)
+                filtered_model = model_content_filters.get(model, None)
 
-            if filtered_model:
-                model = filtered_model
-                logger.info(f"Model changed to {model}")
-            else:
-                logger.info(f"No content filter found for model {model}")
+                if filtered_model:
+                    model = filtered_model
+                    logger.info(f"Model changed to {model}")
+                else:
+                    logger.info(f"No content filter found for model {model}")
 
-        result = self.byteplus_client.images.generate(
-            model=model,
-            prompt=request.prompt,
-            size=request.size,
-            guidance_scale=request.guidance_scale,
-            seed=request.seed,
-            watermark=request.watermark,
-            response_format="url",
-        )
-
-        logger.info(
-            f"BytePlus image generation result:  {result.model_dump()}")
-
-        if result.data is None or len(result.data) == 0:
-            return ImageResponse(
-                error=result.error.model_dump() if result.error else "Unknown error",
-                is_nsfw_detected=result.error.code in BYTEPLUS_NFSW_ERROR_CODES if result.error else False,
-                model_response=result.model_dump()
+            result = self.byteplus_client.images.generate(
+                model=model,
+                prompt=request.prompt,
+                size=request.size,
+                guidance_scale=request.guidance_scale,
+                seed=request.seed,
+                watermark=request.watermark,
+                response_format="url",
             )
 
-        return ImageResponse(
-            asset_urls=[result.data[0].url],
-            model_response=result.model_dump(),
-            model_usage=result.usage.model_dump() if result.usage else None
-        )
+            logger.info(
+                f"BytePlus image generation result:  {result.model_dump()}")
+
+            if result.data is None or len(result.data) == 0:
+                return ImageResponse(
+                    error=result.error.model_dump() if result.error else "Unknown error",
+                    is_nsfw_detected=result.error.code in BYTEPLUS_NFSW_ERROR_CODES if result.error else False,
+                    model_response=result.model_dump()
+                )
+
+            return ImageResponse(
+                asset_urls=[result.data[0].url],
+                model_response=result.model_dump(),
+                model_usage=result.usage.model_dump() if result.usage else None
+            )
+        except ArkBadRequestError as e:
+            logger.error(f"BytePlus BadRequestError: {e}")
+            return ImageResponse(
+                error=str(e),
+                is_nsfw_detected=e.code in BYTEPLUS_NFSW_ERROR_CODES if e.code else False,
+            )
+        except Exception as e:
+            logger.error(f"Error generating image using {request.model}: {e}")
+            return ImageResponse(
+                error=str(e),
+                is_nsfw_detected=False,
+            )
 
     def edit_image(self, request: BytePlusImageEditRequest) -> ImageResponse:
-        model = request.model
-        if request.content_filter_disabled:
-            logger.info(
-                "Changing model configuration: content moderation disabled")
+        try:
+            model = request.model
+            if request.content_filter_disabled:
+                logger.info(
+                    "Changing model configuration: content moderation disabled")
 
-            filtered_model = model_content_filters.get(model, None)
+                filtered_model = model_content_filters.get(model, None)
 
-            if filtered_model:
-                model = filtered_model
-                logger.info(f"Model changed to {model}")
-            else:
-                logger.info(f"No content filter found for model {model}")
+                if filtered_model:
+                    model = filtered_model
+                    logger.info(f"Model changed to {model}")
+                else:
+                    logger.info(f"No content filter found for model {model}")
 
-        result = self.byteplus_client.images.generate(
-            model=model,
-            prompt=request.prompt,
-            size=request.size,
-            guidance_scale=request.guidance_scale,
-            seed=request.seed,
-            watermark=request.watermark,
-            response_format="url",
-            image=BytePlusServiceUtils.convert_url_to_base64_png(request.image)
-        )
-
-        logger.info(
-            f"BytePlus image edit result:  {result.model_dump()}")
-
-        if result.data is None or len(result.data) == 0:
-            return ImageResponse(
-                error=result.error.model_dump() if result.error else "Unknown error",
-                is_nsfw_detected=result.error.code in BYTEPLUS_NFSW_ERROR_CODES if result.error else False,
-                model_response=result.model_dump()
+            result = self.byteplus_client.images.generate(
+                model=model,
+                prompt=request.prompt,
+                size=request.size,
+                guidance_scale=request.guidance_scale,
+                seed=request.seed,
+                watermark=request.watermark,
+                response_format="url",
+                image=BytePlusServiceUtils.convert_url_to_base64_png(
+                    request.image)
             )
 
-        return ImageResponse(
-            asset_urls=[result.data[0].url],
-            model_response=result.model_dump(),
-            model_usage=result.usage.model_dump() if result.usage else None
-        )
+            logger.info(
+                f"BytePlus image edit result:  {result.model_dump()}")
+
+            if result.data is None or len(result.data) == 0:
+                return ImageResponse(
+                    error=result.error.model_dump() if result.error else "Unknown error",
+                    is_nsfw_detected=result.error.code in BYTEPLUS_NFSW_ERROR_CODES if result.error else False,
+                    model_response=result.model_dump()
+                )
+
+            return ImageResponse(
+                asset_urls=[result.data[0].url],
+                model_response=result.model_dump(),
+                model_usage=result.usage.model_dump() if result.usage else None
+            )
+        except ArkBadRequestError as e:
+            logger.error(f"BytePlus BadRequestError: {e}")
+            return ImageResponse(
+                error=str(e),
+                is_nsfw_detected=e.code in BYTEPLUS_NFSW_ERROR_CODES if e.code else False,
+            )
+        except Exception as e:
+            logger.error(f"Error remixing image using {request.model}: {e}")
+            return ImageResponse(
+                error=str(e),
+                is_nsfw_detected=False,
+            )
 
     def generate_image_with_seedream_4(self, request: Seedream4Params) -> ImageResponse:
         try:
@@ -148,22 +176,33 @@ class BytePlusService:
             if response.status_code != 200:
                 logger.error(
                     f"Error in Seedream 4 HTTP response: {response.status_code} - {response.text}")
-                return ImageResponse(
-                    error=f"HTTP {response.status_code}: {response.text}",
-                    is_nsfw_detected=False,
-                )
+
+                if response.status_code == 400:
+                    response = response.json()
+                    error = response.get("error", {})
+                    return ImageResponse(
+                        error=response,
+                        is_nsfw_detected=True if error.get(
+                            "code") in BYTEPLUS_NFSW_ERROR_CODES else False,
+                    )
+                else:
+                    return ImageResponse(
+                        error=f"HTTP {response.status_code}: {response.text}",
+                        is_nsfw_detected=False,
+                    )
 
             result = response.json()
 
             if "data" not in result or not result["data"]:
                 logger.error(
                     f"Error in Seedream 4 response: {result}")
+                data = result.get("data", {})
                 return ImageResponse(
-                    error=result.get("error", "Unknown error"),
+                    error=data.get("error", "Unknown error"),
                     is_nsfw_detected=(
-                        result.get("error", {}).get(
+                        data.get("error", {}).get(
                             "code") in BYTEPLUS_NFSW_ERROR_CODES
-                        if "error" in result else False
+                        if "error" in data else False
                     ),
                 )
 
